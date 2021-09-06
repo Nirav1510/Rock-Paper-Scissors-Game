7c4a8edbae34410645e128715800b0ea3997b993
Nirav <gusainirav1@gmail.com>
2021-09-06 15:59:43 +0530

    Attribution added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <React.Fragment>
+      <footer className="footer">
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Nirav Gusai</a>.
         </div>
-    )
-}
+        <button className="rules">Rules</button>
+      </footer>
+    </React.Fragment>
+  );
+};
 
-export default Footer
+export default Footer;
