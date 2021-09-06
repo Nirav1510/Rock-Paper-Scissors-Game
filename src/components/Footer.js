0c5beaba03ba52bdf2a9677473a891750ba00e3a
Nirav <gusainirav1@gmail.com>
2021-09-06 16:15:12 +0530

    Footer Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Footer = () => {
+  const [modal, setModal] = useState(fasle);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
   return (
     <React.Fragment>
       <footer className="footer">
@@ -9,10 +16,20 @@ const Footer = () => {
           <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
             Frontend Mentor
           </a>
-          . Coded by <a href="#">Nirav Gusai</a>.
+          . Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nirav-gusai-a4627b1a7"
+            target="_blank"
+          >
+            Nirav Gusai
+          </a>
+          .
         </div>
-        <button className="rules">Rules</button>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
       </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
     </React.Fragment>
   );
 };
